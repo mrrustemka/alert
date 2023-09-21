d109930bc27d68b0be328773aa98dd151b41fb5b
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-09-21 20:34:54 +0300

    add-module-css

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './Alert.css';
+import styles from './Alert.module.css';
 
 type Props = {
   type?: string;
@@ -21,25 +21,25 @@ export function Alert({ type = 'information', heading, children, closable, onClo
     }
   }
   return (
-    <div className={`container ${type}`}>
-      <div className="header">
+    <div className={`${styles.container} ${styles[type]}`}>
+      <div className={styles.header}>
         <span
-          className="header-icon"
+          className={styles.headerIcon}
           role="img"
           aria-label={type === 'warning' ? 'Warning' : 'Information'}
         >
           {type === 'warning' ? '⚠' : 'ℹ️'}
         </span>
-        <span className="header-text">{heading}</span>
+        <span className={styles.headerText}>{heading}</span>
         {closable && (
-          <button className="close-button" aria-label="Close" onClick={handleCloseClick}>
+          <button className={styles.closeButton} aria-label="Close" onClick={handleCloseClick}>
             <span role="img" aria-label="Close">
               ❌
             </span>
           </button>
         )}
       </div>
-      <div className="content">{children}</div>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 }
